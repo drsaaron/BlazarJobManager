74483de5ee34019bf1bbc83fd3e89f2124f874f2
Scott Aaron <dr_saaron@yahoo.com>
2019-02-10 09:30:17 -0600

    do not hard-code a properties file for the batch properties

--- src/main/java/com/blazartech/batch/impl/spring/config/JobRepositoryDataSourceConfiguration.java
+++ src/main/java/com/blazartech/batch/impl/spring/config/JobRepositoryDataSourceConfiguration.java
@@ -13,14 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  *
  * @author AAR1069
  */
 @Configuration
-@PropertySource("file:${user.home}/.blazartech/batch.properties")
 public class JobRepositoryDataSourceConfiguration {
 
     private static final Logger logger = Logger.getLogger(JobRepositoryDataSourceConfiguration.class);
