c0a91246de9721273ce409f59435e4560880cea0
Scott Aaron <dr_saaron@yahoo.com>
2025-08-27 14:30:08 -0500

    fix creation of jobRepository bean

--- src/main/java/com/blazartech/batch/impl/spring/config/JobRepositoryConfiguration.java
+++ src/main/java/com/blazartech/batch/impl/spring/config/JobRepositoryConfiguration.java
@@ -6,7 +6,10 @@
 package com.blazartech.batch.impl.spring.config;
 
 import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.repository.ExecutionContextSerializer;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -22,6 +25,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 public class JobRepositoryConfiguration {
 
+    private static final Logger logger = LoggerFactory.getLogger(JobRepositoryConfiguration.class);
+    
     /*
     <batch:job-repository data-source="JobRepositoryDataSource" transaction-manager="transactionManager" serializer="batchDefaultSerializer" /> 
      */
@@ -40,12 +45,14 @@ public class JobRepositoryConfiguration {
     private String tablePrefix;
 
     @Bean
-    public JobRepositoryFactoryBean jobRepository() {
+    public JobRepository jobRepository() throws Exception {
+        logger.info("building job repo");
         JobRepositoryFactoryBean jr = new JobRepositoryFactoryBean();
         jr.setDataSource(jobRepositoryDataSource);
         jr.setSerializer(batchDefaultSerializer);
         jr.setTransactionManager(jobRepositoryTransactionManager);
         jr.setTablePrefix(tablePrefix);
-        return jr;
+        jr.afterPropertiesSet();
+        return jr.getObject();
     }
 }
