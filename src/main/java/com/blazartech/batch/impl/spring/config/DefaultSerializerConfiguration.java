4e59b115e238f6d5219671b6e4c0e6af361e31fe
Scott Aaron <dr_saaron@yahoo.com>
2025-12-02 17:15:54 -0600

    use the new jackson execution context serializer; users will need to cear the repo before first run

--- src/main/java/com/blazartech/batch/impl/spring/config/DefaultSerializerConfiguration.java
+++ src/main/java/com/blazartech/batch/impl/spring/config/DefaultSerializerConfiguration.java
@@ -5,9 +5,13 @@
  */
 package com.blazartech.batch.impl.spring.config;
 
-import org.springframework.batch.core.repository.dao.Jackson2ExecutionContextStringSerializer;
+import org.springframework.batch.core.repository.ExecutionContextSerializer;
+import org.springframework.batch.core.repository.dao.JacksonExecutionContextStringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tools.jackson.databind.json.JsonMapper;
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import tools.jackson.databind.jsontype.PolymorphicTypeValidator;
 
 /**
  *
@@ -17,7 +21,23 @@ import org.springframework.context.annotation.Configuration;
 public class DefaultSerializerConfiguration {
     
     @Bean
-    public Jackson2ExecutionContextStringSerializer batchDefaultSerializer() {
-        return new Jackson2ExecutionContextStringSerializer();
+    public ExecutionContextSerializer batchDefaultSerializer() {
+        /* going to use the jackson serializer, but we need to include com.blazrtech
+           objects.  So basically do what the default constuctor
+           does and include our own types.
+        */
+        PolymorphicTypeValidator b = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType("java.util.")
+                .allowIfSubType("java.sql.")
+                .allowIfSubType("java.lang.")
+                .allowIfSubType("java.math.")
+                .allowIfSubType("java.time.")
+                .allowIfSubType("java.net.")
+                .allowIfSubType("java.xml.")
+                .allowIfSubType("org.springframework.batch.")
+                .allowIfSubType("com.blazartech.")
+                .build();
+        JsonMapper m = JsonMapper.builder().activateDefaultTyping(b).build();
+        return new JacksonExecutionContextStringSerializer(m);
     }
 }
